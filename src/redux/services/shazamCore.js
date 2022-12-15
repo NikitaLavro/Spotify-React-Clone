a5464e83f95cb05d75037e72f2c1df6ef7bb70d0
Nikita <lavro.nikita123@gmail.com>
2022-12-15 13:36:30 -0500

    ShazamCore slice initial set up

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,18 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "f745072f70msh4afa46d5a37fb45p13b8a6jsn79315a545e0a",
+    "X-RapidAPI-Host": "shazam-core.p.rapidapi.com",
+  },
+};
+
+fetch("https://shazam-core.p.rapidapi.com/v1/charts/world", options)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+});

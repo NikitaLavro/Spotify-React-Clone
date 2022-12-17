00ae2fee9fa49c488061dc26dea5595e42565f4b
Nikita <lavro.nikita123@gmail.com>
2022-12-17 16:44:08 -0500

    TopPlay component imports

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -1,4 +1,21 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+
+//Router
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+
+//Swiper
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode } from "swiper";
+import "swiper/css";
+import "swiper/css/free-mode";
+
+//Components
+import PlayPause from "./PlayPause";
+
+//Redux
+import { playPause, setActiveSong } from "../redux/features/playerSlice";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 
 const TopPlay = () => <div>TopPlay</div>;
 

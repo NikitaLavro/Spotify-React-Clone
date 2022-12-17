a137d995b9d5fe61d230c40cfaef4278ff7a1844
Nikita <lavro.nikita123@gmail.com>
2022-12-17 16:08:53 -0500

    sideBar inputs and mobile State

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Sidebar = () => <div>Sidebar</div>;
+//Router
+import { NavLink } from "react-router-dom";
+
+//Icons
+import { RiCloseLine } from "react-icons/ri";
+
+//Images
+import { logo } from "../assets";
+
+//Links
+import { links } from "../assets/constants";
+
+const Sidebar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return <></>;
+};
 
 export default Sidebar;

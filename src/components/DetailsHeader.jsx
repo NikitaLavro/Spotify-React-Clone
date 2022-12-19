dc0fbdd31f71b34a4d39fc01b104ce867c1ef154
Nikita <lavro.nikita123@gmail.com>
2022-12-19 08:06:06 -0500

    DetailsHeader complete

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -20,9 +20,28 @@ const DetailsHeader = ({ artistId, artistData, songData }) => {
           alt="art"
         />
         <div className="ml-5">
-          <p>{artistId ? artist.name : songData?.title}</p>
+          <p className="font-bold sm:text-3xl text-xl text-white">
+            {artistId ? artist.name : songData?.title}
+          </p>
+          {!artistId && (
+            <Link
+              to={
+                songData?.artists
+                  ? `/artists/${songData.artists[0].adamid}`
+                  : `/artists/no-artist`
+              }
+            >
+              <p className="text-base text-gray-400 mt-2">
+                {songData?.subtitle}
+              </p>
+            </Link>
+          )}
+          <p className="text-base text-gray-400 mt-2">
+            {artistId ? artist?.genreNames[0] : songData?.genres.primary}
+          </p>
         </div>
       </div>
+      <div className="w-full sm:h-44 h-24" />
     </div>
   );
 };

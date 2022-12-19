a7787e87d782010eb71758bdddbad2aecb9aab37
Nikita <lavro.nikita123@gmail.com>
2022-12-19 07:28:18 -0500

    DetailsHeader image JSX

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -4,7 +4,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const DetailsHeader = ({ artistId, artistData, songData }) => (
-  <div>DetailsHeader</div>
+  <div className="relative w-full flex flex-col">
+    <div className="w-full bg-gradient-to-l from-transparent to-black sm:h-48 h-28" />
+    <div className="absolute inset-0 flex items-center">
+      <img
+        src={
+          artistId
+            ? artistData?.artist.artist[artistId].attributes?.artwork?.url
+                .replace("{w}", "500")
+                .replace("{h}", "500")
+            : songData?.images?.coverart
+        }
+        alt="art"
+      />
+    </div>
+  </div>
 );
 
 export default DetailsHeader;

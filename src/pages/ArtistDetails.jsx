0aeed696f90a50613b4568a0e9daf300d93a9e0c
Nikita <lavro.nikita123@gmail.com>
2022-12-20 17:12:49 -0500

    ArtistDetails return JSX refactoring

--- src/pages/ArtistDetails.jsx
+++ src/pages/ArtistDetails.jsx
@@ -10,7 +10,7 @@ import { useGetArtistDetailsQuery } from "../redux/services/shazamCore";
 //Components
 import { DetailsHeader, Error, Loader, RelatedSongs } from "../components";
 
-const ArtistDetils = () => {
+const ArtistDetails = () => {
   const { artistId } = useParams();
   const { activeSong, isPlaying } = useSelector((state) => state.player);
 
@@ -18,34 +18,20 @@ const ArtistDetils = () => {
     data: artistData,
     isFetching: isFetchingArtistDetails,
     error,
-  } = useGetSongDetailsQuery(artistId);
+  } = useGetArtistDetailsQuery(artistId);
 
   if (isFetchingArtistDetails) return <Loader title="Loading artist details" />;
 
   if (error) return <Error />;
   return (
     <div className="flex flex-col">
-      <DetailsHeader artistId="" songData={songData} />
-      <div className="mb-10">
-        <h2 className="text-white text-3xl font-bold">Lyrics</h2>
-        <div className="mt-5">
-          {songData?.sections[1].type === "LYRICS" ? (
-            songData?.sections[1].text.map((line, i) => (
-              <p className="text-gray-400 text-base my-1">{line}</p>
-            ))
-          ) : (
-            <p className="text-gray-400 text-base my-1">
-              Sorry, no lyrics found
-            </p>
-          )}
-        </div>
-      </div>
+      <DetailsHeader artistId={artistId} artistData={artistData} />
+
       <RelatedSongs
-        data={data}
+        data={Object.values(artistData?.songs)}
+        artistId={artistId}
         isPlaying={isPlaying}
         activeSong={activeSong}
-        handlePauseClick={handlePauseClick}
-        handlePlayClick={handlePlayClick}
       />
     </div>
   );
